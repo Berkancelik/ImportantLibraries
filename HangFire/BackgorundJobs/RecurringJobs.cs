a6236c9fb3db47069fbf4d5e7732e5c6f0bcf3ef
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-13 09:37:19 +0300

    Recurring Jobs

---
+++ HangFire/BackgorundJobs/RecurringJobs.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire;
+using System.Diagnostics;
+
+namespace HangFire.BackgorundJobs
+{
+    public class RecurringJobs
+    {
+        public static void ReportingJob()
+        {
+            Hangfire.RecurringJob.AddOrUpdate("reportjob1", () => EmailReport(),Cron.Minutely);
+        }
+        public static void EmailReport()
+        {
+            Debug.WriteLine("Rapor, email olarak gönderildi.");
+        }
+    }
+}
--- HangFire/Controllers/HomeController.cs
+++ HangFire/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using HangFire.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,6 +34,33 @@ namespace HangFire.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+       
+        public IActionResult PictureSave()
+        {
+            BackgorundJobs.RecurringJobs.ReportingJob();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PictureSave(IFormFile picture)
+        {
+            string newFileName = String.Empty;
+            if (picture != null && picture.Length > 0)
+            {
+                newFileName = Guid.NewGuid().ToString() + Path.GetExtension(picture.FileName);
+            }
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pictures", newFileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await picture.CopyToAsync(stream);
+            }
+            string jobID = BackgorundJobs.DelayedJobs.AddWaterMarkJob(newFileName, "www.mysite.com");
+            return View();
+
         }
     }
 }
